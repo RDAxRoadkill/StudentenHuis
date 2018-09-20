064887b46139a207df224236830230cafe8d4166
Wouter <wouter97@planet.nl>
2018-09-20 15:04:06 +0200

    Added dev branch

--- StudHuis/Models/IMealRepository.cs
+++ StudHuis/Models/IMealRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace StudHuis.Models
+﻿using System.Linq;
+
+namespace StudHuis.Models
 {
     public class IMealRepository
     {
-        
+        private IQueryable<Meal> Meals { get;  }
     }
 }
